7aa5d7cd6b91750e33cf564314f3288a9914d762
myllyms3 <saku.myllymaki@aalto.fi>
2019-11-02 15:15:28 +0200

    restrict map size

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import './App.css';
 
 const App = () => {
 
-  const position = [51.505, -0.09];
+  const position = [60.17, 24.935];
 
   return (
     <div className="App">
       <h2 className='AppHeader'>Ran Ranin karttapalvelut</h2>
-      <Map center={position} zoom={13}>
+      <Map id='Map' center={position} zoom={13}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         />
+        <Marker position={position}>
+          <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+        </Marker>
       </Map>
     </div>
   );
