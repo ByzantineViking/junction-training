7271fd417d5607043223773c79e17625784faf24
myllyms3 <saku.myllymaki@aalto.fi>
2019-11-02 13:12:16 +0200

    initial frontpage

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className='AppHeader'>Ran Ranin karttapalvelut</h2>
+    </div>
+  );
+}
+
+export default App;
