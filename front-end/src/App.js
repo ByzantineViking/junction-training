88237ddb53ad76c786341c174a55766e5218686c
myllyms3 <saku.myllymaki@aalto.fi>
2019-11-02 16:25:43 +0200

    get markers from api

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { getData } from './services/Server';
 import './App.css';
 
 const App = () => {
 
   const Helsinki = [60.170833, 24.9375];
-  const mockLocations = [
-    Helsinki,
-    [61.162253,24.922169],
-    [62.155534,24.924101],
-    [63.147018, 24.913987],
-    [64.151775,24.903101],
-    [65.155356,24.901513],
-    [66.158474,24.90265],
-    [67.161862,24.914016]
-  ];
-  const [locations, setLocations] = useState([]);
+  
+  const [markers, setMarkers] = useState([]);
   useEffect(() => {
-    setLocations(mockLocations);
+    getData().then(response => setMarkers(response))
   }, []);
   return (
     <div className="App">
@@ -27,8 +19,8 @@ const App = () => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         />
-        {locations.map(loc => {
-          return <Marker position={loc}>
+        {markers.map(marker => {
+          return <Marker position={[marker.lon, marker.lat]}>
             <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
           </Marker>
         })}
--- front-end/src/services/Server.js
+++ front-end/src/services/Server.js
@@ -1,8 +1,5 @@
 const axios = require('axios').default;
 
-export async function getData(){
-    const response = await axios.get('http://open-api.myhelsinki.fi/v1/places/?tags_filter=Restaurant')
-    return response.data.data.map(z => z.location))
+export function getData(){
+    return axios.get('http://open-api.myhelsinki.fi/v1/places/?tags_filter=Restaurant')
 }
-
-export default location services
