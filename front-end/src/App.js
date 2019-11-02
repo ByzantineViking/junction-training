ec5bf9ce66e0abe4da3a0d80e7dafbcd25cdc076
myllyms3 <saku.myllymaki@aalto.fi>
2019-11-02 14:11:08 +0200

    add leaflet

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Map, TileLayer } from 'react-leaflet';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const position = [51.505, -0.09];
+
   return (
     <div className="App">
       <h2 className='AppHeader'>Ran Ranin karttapalvelut</h2>
+      <Map center={position} zoom={13}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        />
+      </Map>
     </div>
   );
 }
